b1fbd1155aea9b19af6d6b968cf3c3af62160282
Dev <devinlgar@gmail.com>
2022-03-20 16:47:37 -0400

    global variables and arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,39 @@
-function generatePassword() {
-    // How many characters 
-    var passLength = window.prompt("How many characters would you like the password to be?");
-    if(isNaN(passLength)) {
-      window.alert("Input a numeric value");
-    }
-    else if (passLength < 8 || passLength > 50) {
-      window.alert("Password length must be 8-50 characters");
-    }
-    // confirm uppercase, lowercase, numeric, special characters
-    else {
-      confirmUpper = confirm("Will the password contain uppercase letters?")
-      confirmLower = confirm("Will the password contain lowercase letters?")
-      confirmNum = confirm("Will the password contain numbers?")
-      confirmSpec = confirm("Will the password contain special characters?")
-    };
+var passLength;
+var confirmUpper;
+var confirmLower;
+var confirmNum;
+var confirmSpec;
 
-    // must select atleast one character type
-    if (!confirmUpper && !confirmLower && !confirmNum && !confirmSpec) {
-      window.alert("You must choose atleast one criteria.")
-    }
+specChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"]
+
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+letterLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+letterUp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+function generatePassword() {
+  // How many characters 
+  var passLength = window.prompt("How many characters would you like the password to be?");
+  if (isNaN(passLength)) {
+    window.alert("Input a numeric value");
+  }
+  else if (passLength < 8 || passLength > 50) {
+    window.alert("Password length must be 8-50 characters");
+  }
+  // confirm uppercase, lowercase, numeric, special characters
+  else {
+    confirmUpper = confirm("Will the password contain uppercase letters?")
+    confirmLower = confirm("Will the password contain lowercase letters?")
+    confirmNum = confirm("Will the password contain numbers?")
+    confirmSpec = confirm("Will the password contain special characters?")
+  };
+
+  // must select atleast one character type
+  if (!confirmUpper && !confirmLower && !confirmNum && !confirmSpec) {
+    window.alert("You must choose atleast one criteria.")
+  }
+
 }
 
 // Get references to the #generate element
