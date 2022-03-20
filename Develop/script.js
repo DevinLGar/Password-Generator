d59fddf1dbaafe7c082f25c4b6c5d52a2149dcca
Dev <devinlgar@gmail.com>
2022-03-20 16:18:15 -0400

    generatePassword defined with empty prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,33 @@
-// Assignment code here
+function generatePassword() {
+    // How many characters 
+    var passLength = window.prompt("How many characters would you like the password to be?");
+    if(isNaN(passLength)) {
+      window.alert("Input a numeric value");
+    }
+    else if (passLength < 8 || passLength > 50) {
+      window.alert("Password length must be 8-50 characters");
+    }
+    // confirm uppercase, lowercase, numeric, special characters
+    else {
+      confirmUpper = confirm("Will the password contain uppercase letters?")
+      confirmLower = confirm("Will the password contain lowercase letters?")
+      confirmNum = confirm("Will the password contain numbers?")
+      confirmSpec = confirm("Will the password contain special characters?")
+    };
 
+    // must select atleast one character type
+    if (!confirmUpper && !confirmLower && !confirmNum && !confirmSpec) {
+      window.alert("You must choose atleast one criteria.")
+    }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -15,3 +37,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Password criteria prompts
+// *length criteria (8, 128)
+// *lowercase, uppercase, etc
